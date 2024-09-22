f07ecb7212317f2baafed7ac439a4e835d424f5e
ndamatta <damatta.natanael@gmail.com>
2024-09-22 13:08:02 -0300

    add welcome msg to test index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const contactsRoute = require('./contacts')
 
+router.get('/', (req, res) => res.send("Index page is working fine :)"))
 router.use('/contacts', contactsRoute)
 
 module.exports = router;
