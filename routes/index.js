40f12575e97112873e0fd6f1b41c23800eeb368a
ndamatta <damatta.natanael@gmail.com>
2024-09-27 21:31:20 -0300

    add swagger

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 const contactsRoute = require('./contacts')
+const swaggerRoute = require('./swagger')
+
+router.use('/', swaggerRoute);
+router.get('/', (req, res) => {res.send('Hello world')})
 
 router.get('/', (req, res) => res.send("Index page is working fine :)"))
 router.use('/contacts', contactsRoute)
--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Contacts API'
     },
     host: 'localhost:3000',
-    schemes: ['https']
+    schemes: ['https', 'http']
 };
 
 const outputFile = './swagger.json';
