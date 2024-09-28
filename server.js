463927e021e357be7875dd6ae1df2ea1437ce563
ndamatta <damatta.natanael@gmail.com>
2024-09-27 21:29:56 -0300

    added headers

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const indexRoute = require('./routes')
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
+app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 app.use('/', indexRoute);
 
 mongodb.initDb((err) => {
